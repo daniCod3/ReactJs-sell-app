348bb695b1dee4b7336e30a3cb750cbc7e6e4626
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-08-30 11:13:35 -0600

    09

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,11 +6,6 @@ export type Guitar = {
     price: number
 }
 
-export type CartItem = {
-    id: number
-    name: string
-    image: string
-    description: string
-    price: number
+export type CartItem = Guitar & {
     quantity: number
 }
